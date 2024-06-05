d6364118f63751c381b9b0dcd48d4ec4a360d51c
Asif Mahmood <asifmehmood.8270@gmail.com>
2024-06-05 14:30:11 +0500

    Removed lis2dh12_reg.h and added it to lis2dh12_reg.h

--- components/lis2dh12/lis2dh12.c
+++ components/lis2dh12/lis2dh12.c
@@ -12,12 +12,22 @@
 #include <stdbool.h>
 #include <stdint.h>
 #include <string.h>
+#include <zephyr/kernel.h>
+#include <zephyr/device.h>
 
 #include "lis2dh12.h"
 #include "twi.h"
 
 int lis2dh12_init()
 {
+    uint8_t ctrl_reg = 0x57U;
+    int err;
+    err = twi_write(LIS2DH12_I2C_ADDR, LIS2DH12_CTRL_REG1, &ctrl_reg, sizeof(ctrl_reg));
+    if (err != 0)
+    {
+        printk("\rFailed to twi_write");
+        return 0;
+    }
     
     return 0;
 }
--- components/lis2dh12/lis2dh12.h
+++ components/lis2dh12/lis2dh12.h
@@ -16,6 +16,47 @@
 #include <stdbool.h>
 #include <stdint.h>
 
+// Register addresses
+#define LIS2DH12_I2C_ADDR         0x18
+#define LIS2DH12_STATUS_REG_AUX   0x07
+#define LIS2DH12_OUT_TEMP_L       0x0C
+#define LIS2DH12_OUT_TEMP_H       0x0D
+#define LIS2DH12_WHO_AM_I         0x0F
+#define LIS2DH12_CTRL_REG0        0x1E
+#define LIS2DH12_TEMP_CFG_REG     0x1F
+#define LIS2DH12_CTRL_REG1        0x20
+#define LIS2DH12_CTRL_REG2        0x21
+#define LIS2DH12_CTRL_REG3        0x22
+#define LIS2DH12_CTRL_REG4        0x23
+#define LIS2DH12_CTRL_REG5        0x24
+#define LIS2DH12_CTRL_REG6        0x25
+#define LIS2DH12_REFERENCE        0x26
+#define LIS2DH12_STATUS_REG       0x27
+#define LIS2DH12_OUT_X_L          0x28
+#define LIS2DH12_OUT_X_H          0x29
+#define LIS2DH12_OUT_Y_L          0x2A
+#define LIS2DH12_OUT_Y_H          0x2B
+#define LIS2DH12_OUT_Z_L          0x2C
+#define LIS2DH12_OUT_Z_H          0x2D
+#define LIS2DH12_FIFO_CTRL_REG    0x2E
+#define LIS2DH12_FIFO_SRC_REG     0x2F
+#define LIS2DH12_INT1_CFG         0x30
+#define LIS2DH12_INT1_SRC         0x31
+#define LIS2DH12_INT1_THS         0x32
+#define LIS2DH12_INT1_DURATION    0x33
+#define LIS2DH12_INT2_CFG         0x34
+#define LIS2DH12_INT2_SRC         0x35
+#define LIS2DH12_INT2_THS         0x36
+#define LIS2DH12_INT2_DURATION    0x37
+#define LIS2DH12_CLICK_CFG        0x38
+#define LIS2DH12_CLICK_SRC        0x39
+#define LIS2DH12_CLICK_THS        0x3A
+#define LIS2DH12_TIME_LIMIT       0x3B
+#define LIS2DH12_TIME_LATENCY     0x3C
+#define LIS2DH12_TIME_WINDOW      0x3D
+#define LIS2DH12_ACT_THS          0x3E
+#define LIS2DH12_ACT_DUR          0x3F
+
 /**@brief Enum defining Accelerometer's Full Scale range posibillities in Gs. */
 enum accel_range
 {
